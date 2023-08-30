7bac45f2bb38e4ccbdfde516caaa43bd24fb85b2
Kyle Denis <kylenden@gmail.com>
2023-08-30 13:36:12 +1000

    Added checkbox

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
 import List from './components/List';
 import './App.css';
 
+const tasks = [
+    { description: 'Learn React', completed: true },
+    { description: 'Learn JSX', completed: true },
+    { description: 'Build a React App', completed: false },
+];
+
 function App() {
     return (
         <div className='page'>     
-            <List heading='My Tasks'/>
+            <List heading='My Tasks' tasks={tasks}/>
         </div>
     );
 }
--- src/components/List.js
+++ src/components/List.js
@@ -1,11 +1,23 @@
+function Task(props) {
+	return (
+		<li>
+			{props.description}
+			<input type="checkbox" checked={props.completed} readOnly/>
+		</li>
+	);
+}
+
 function List(props) {
 	return (
 		<div>
 			<h1>{ props.heading }</h1>
 			<ul>
-				<li>Learn React</li>
-				<li>Learn JSX</li>
-				<li>Build a React App</li>
+				{/* {tasks} */}
+				{props.tasks.map(task =>
+					<Task
+						description = {task.description}
+						completed = {task.completed}/>
+				)}
 			</ul>
 		</div>
 	);
